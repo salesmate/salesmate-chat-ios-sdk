fb6e5f8c050b43d61fd51336df97b6693e150e5e
Mehul Thakkar <mehul.thakkar@rapidops.com>
2021-10-18 15:35:36 +0530

    Updating the project structure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # salesmate-chat-ios-sdk
+
+Installation Steps:
+
+1. Configure Messenger at your salesmate.io link(For more detail visit: https://support.salesmate.io/hc/en-us/categories/360005786852-Messenger)
+
+2. After successfull configuration, go to profile -> Setup -> Messenger(from App & Add-Ons sections) -> Installation
+
+Here you will find these 3 things: 'workspace_id', 'app_key' and 'tenant_id'. Copy that.
+
+Now, in your iOS Code, Open appDelegate file, and 
--- SalesmateChatSDK/SalesmateChatSDK/TestApp/ViewController.swift
+++ SalesmateChatSDK/SalesmateChatSDK/TestApp/ViewController.swift
@@ -13,11 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let setting = Settings(workspaceID: "3ade8edc-5a62-45dc-b7a5-46b0e40ffb57",
-                               appKey: "2f33c730-b08a-11eb-99e2-f3b202d2d81c",
-                               tenantID: "dev18.salesmate.io")
-
-        SalesmateChat.setSalesmateChat(configeration: setting)
+        self.configureSalesmate()
 
 //        if let id = UIDevice.current.identifierForVendor?.uuidString {
 //            SalesmateChat.setVerifiedID(id)
@@ -32,5 +28,13 @@ class ViewController: UIViewController {
 
     @IBAction func showChatPressed(_ sender: UIButton) {
         SalesmateChat.presentMessenger(from: self)
+    }
+    
+    func configureSalesmate(){
+        let setting = Settings(workspaceID: "3ade8edc-5a62-45dc-b7a5-46b0e40ffb57",
+                               appKey: "2f33c730-b08a-11eb-99e2-f3b202d2d81c",
+                               tenantID: "dev18.salesmate.io")
+
+        SalesmateChat.setSalesmateChat(configeration: setting)
     }
 }
