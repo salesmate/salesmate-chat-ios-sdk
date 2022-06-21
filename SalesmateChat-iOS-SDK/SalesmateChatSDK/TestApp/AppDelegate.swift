f8c974592d20c63d038d6ab97a38e1606de1765e
Vishal Nandoriya <vishal.nandoriya@rapidops.com>
2022-06-21 11:49:25 +0530

    Added Keyboard manager.

--- SalesmateChat-iOS-SDK/SalesmateChatSDK/TestApp/AppDelegate.swift
+++ SalesmateChat-iOS-SDK/SalesmateChatSDK/TestApp/AppDelegate.swift
@@ -10,4 +10,12 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+      IQKeyboardManager.shared.enable = true
+
+      return true
+    }
+
 }
